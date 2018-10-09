75664e67c10f1481f33120235919a6e858d39678
nekr0s <alexanderrusev@abv.bg>
2018-10-09 12:18:17 +0300

    Working address

--- app/src/main/java/com/example/nekr0s/hackathon/ContactsActivity.java
+++ app/src/main/java/com/example/nekr0s/hackathon/ContactsActivity.java
@@ -20,37 +20,35 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class ContactsActivity extends AppCompatActivity
-{
+public class ContactsActivity extends AppCompatActivity {
     private static final int REQUEST_PERMISSION_CODE = 1;
-    
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         ListView listView = findViewById(R.id.lv_contacts);
-        
+
         enableRuntimePermission();
-        
+
         CustomAdapter arrayAdapter = new CustomAdapter(
                 ContactsActivity.this,
                 getContacts()
         );
-        
+
         listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener((parent, view, position, id) ->
         {
             Contact contact = getContacts().get(position);
             String address = contact.getAddress();
-            
+
             Intent intent = new Intent(this, MapActivity.class);
             intent.putExtra(Constants.LOCATION_NAME_DATA_EXTRA, address);
-            
+
             int serviceAvailability =
                     GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
-            
+
             if (serviceAvailability == ConnectionResult.SUCCESS)
                 startActivity(intent);
             else
@@ -60,52 +58,44 @@ public class ContactsActivity extends AppCompatActivity
                         .show();
         });
     }
-    
-    private List<Contact> getContacts()
-    {
+
+    private List<Contact> getContacts() {
         ArrayList<Contact> contacts = new ArrayList<>();
         Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                 null, null, null, null);
-        while (Objects.requireNonNull(cursor).moveToNext())
-        {
+        Cursor cursor2 = getContentResolver().query(ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_URI,
+                null, null, null, null);
+        while (Objects.requireNonNull(cursor).moveToNext() &&Objects.requireNonNull(cursor2).moveToNext()) {
             String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
             String phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-            String address = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.FORMATTED_ADDRESS));
+            String address = cursor2.getString(cursor2.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.FORMATTED_ADDRESS));
             contacts.add(new Contact(name, phoneNumber, address));
         }
         cursor.close();
-        
         return contacts;
     }
-    
-    private void enableRuntimePermission()
-    {
+
+    private void enableRuntimePermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(
                 ContactsActivity.this,
-                Manifest.permission.READ_CONTACTS))
-        {
+                Manifest.permission.READ_CONTACTS)) {
             Toast.makeText(ContactsActivity.this,
                     "CONTACTS permission allows us to Access CONTACTS app",
                     Toast.LENGTH_LONG)
                     .show();
-        } else
-        {
+        } else {
             ActivityCompat.requestPermissions(ContactsActivity.this, new String[]{
                     Manifest.permission.READ_CONTACTS}, REQUEST_PERMISSION_CODE);
         }
     }
-    
+
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-    {
-        switch (requestCode)
-        {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
             case REQUEST_PERMISSION_CODE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(this, "Permission Granted.", Toast.LENGTH_SHORT).show();
-                } else
-                {
+                } else {
                     Toast.makeText(this, "Permission Denied.", Toast.LENGTH_SHORT).show();
                 }
                 break;
