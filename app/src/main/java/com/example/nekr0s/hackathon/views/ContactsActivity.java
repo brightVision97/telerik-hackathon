19e37f6d31412c4efeb5b256634689be04a681d2
Ivan Rachev <ivan.rachev97@gmail.com>
2018-10-09 16:07:42 +0300

    updated project to use dexter permissions

--- app/src/main/java/com/example/nekr0s/hackathon/views/ContactsActivity.java
+++ app/src/main/java/com/example/nekr0s/hackathon/views/ContactsActivity.java
@@ -2,11 +2,8 @@ package com.example.nekr0s.hackathon.views;
 
 import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.*;
@@ -17,40 +14,46 @@ import com.example.nekr0s.hackathon.adapter.CustomAdapter;
 import com.example.nekr0s.hackathon.models.Contact;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class ContactsActivity extends AppCompatActivity {
-    private static final int REQUEST_PERMISSION_CODE = 1;
-
+public class ContactsActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         ListView listView = findViewById(R.id.lv_contacts);
-
-        enableRuntimePermission();
-
+        
+        getContactsPermission();
+        
         CustomAdapter arrayAdapter = new CustomAdapter(
                 ContactsActivity.this,
                 getContacts()
         );
-
+        
         listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener((parent, view, position, id) ->
         {
             Contact contact = getContacts().get(position);
             String address = contact.getAddress();
-
+            
             Intent intent = new Intent(this, MapActivity.class);
             intent.putExtra(Constants.LOCATION_NAME_DATA_EXTRA, address);
-
+            
             int serviceAvailability =
                     GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
-
+            
             if (serviceAvailability == ConnectionResult.SUCCESS)
                 startActivity(intent);
             else
@@ -60,14 +63,16 @@ public class ContactsActivity extends AppCompatActivity {
                         .show();
         });
     }
-
-    private List<Contact> getContacts() {
+    
+    private List<Contact> getContacts()
+    {
         ArrayList<Contact> contacts = new ArrayList<>();
         Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                 null, null, null, null);
         Cursor cursor2 = getContentResolver().query(ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_URI,
                 null, null, null, null);
-        while (Objects.requireNonNull(cursor).moveToNext() && Objects.requireNonNull(cursor2).moveToNext()) {
+        while (Objects.requireNonNull(cursor).moveToNext() && Objects.requireNonNull(cursor2).moveToNext())
+        {
             String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
             String phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
             String address = cursor2.getString(cursor2.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.FORMATTED_ADDRESS));
@@ -76,31 +81,39 @@ public class ContactsActivity extends AppCompatActivity {
         cursor.close();
         return contacts;
     }
-
-    private void enableRuntimePermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(
-                ContactsActivity.this,
-                Manifest.permission.READ_CONTACTS)) {
-            Toast.makeText(ContactsActivity.this,
-                    "CONTACTS permission allows us to Access CONTACTS app",
-                    Toast.LENGTH_LONG)
-                    .show();
-        } else {
-            ActivityCompat.requestPermissions(ContactsActivity.this, new String[]{
-                    Manifest.permission.READ_CONTACTS}, REQUEST_PERMISSION_CODE);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_PERMISSION_CODE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Permission Granted.", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this, "Permission Denied.", Toast.LENGTH_SHORT).show();
-                }
-                break;
-        }
+    
+    private void getContactsPermission()
+    {
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.READ_CONTACTS)
+                .withListener(new PermissionListener()
+                {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response)
+                    {
+                        Toast.makeText(getApplicationContext(),
+                                "Permission granted",
+                                Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                    
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response)
+                    {
+                        Toast.makeText(getApplicationContext(),
+                                "Permission denied",
+                                Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                    
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission,
+                                                                   PermissionToken token)
+                    {
+                        token.continuePermissionRequest();
+                    }
+                })
+                .onSameThread()
+                .check();
     }
 }
--- app/src/main/java/com/example/nekr0s/hackathon/views/MapActivity.java
+++ app/src/main/java/com/example/nekr0s/hackathon/views/MapActivity.java
@@ -5,90 +5,135 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.widget.Toast;
 import com.example.nekr0s.hackathon.Constants;
 import com.example.nekr0s.hackathon.R;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
+{
     private GoogleMap mMap;
-
+    
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(com.example.nekr0s.hackathon.R.layout.activity_map);
-
+        
         getLocationPermission();
-
+        
         initializeMap();
     }
-
-    private void geoLocate(String locationName) {
+    
+    private void geoLocate(String locationName)
+    {
         Geocoder geocoder = new Geocoder(MapActivity.this);
         List<Address> list = new ArrayList<>();
-
-        try {
+        
+        try
+        {
             list = geocoder.getFromLocationName(locationName, 1);
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             e.printStackTrace();
         }
-
-        if (list.size() > 0) {
+        
+        if (list.size() > 0)
+        {
             Address address = list.get(0);
-
+            
             moveCamera(new LatLng(address.getLatitude(), address.getLongitude()),
                     address.getAddressLine(0));
         }
     }
-
+    
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap)
+    {
         mMap = googleMap;
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+        
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
             return;
-
+        
         mMap.setMyLocationEnabled(true);
         mMap.getUiSettings().setMyLocationButtonEnabled(false);
-
+        
         geoLocate(getIntent().getStringExtra(Constants.LOCATION_NAME_DATA_EXTRA));
     }
-
-    private void moveCamera(LatLng latLng, String title) {
+    
+    private void moveCamera(LatLng latLng, String title)
+    {
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, Constants.DEFAULT_ZOOM));
-
+        
         MarkerOptions options = new MarkerOptions()
                 .position(latLng)
                 .title(title);
-
+        
         mMap.addMarker(options);
     }
-
-    private void initializeMap() {
+    
+    private void initializeMap()
+    {
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-
-        Objects.requireNonNull(mapFragment).getMapAsync(MapActivity.this);
+        
+        Objects.requireNonNull(mapFragment).getMapAsync(this);
     }
-
-    private void getLocationPermission() {
-        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION};
-
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                Constants.FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-            ActivityCompat.requestPermissions(this, permissions,
-                    Constants.LOCATION_PERMISSION_REQUEST_CODE);
+    
+    private void getLocationPermission()
+    {
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                .withListener(new MultiplePermissionsListener()
+                {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report)
+                    {
+                        if (report.areAllPermissionsGranted())
+                        {
+                            Toast.makeText(getApplicationContext(),
+                                    "Permissions granted",
+                                    Toast.LENGTH_SHORT)
+                                    .show();
+                        } else
+                            Toast.makeText(getApplicationContext(),
+                                    "Permissions denied",
+                                    Toast.LENGTH_SHORT)
+                                    .show();
+                        
+                        if (report.isAnyPermissionPermanentlyDenied())
+                            Toast.makeText(getApplicationContext(),
+                                    "Permissions permanently denied",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                    }
+                    
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions,
+                                                                   PermissionToken token)
+                    {
+                        token.continuePermissionRequest();
+                    }
+                })
+                .onSameThread()
+                .check();
     }
 }
